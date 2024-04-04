d2e98c22fe872c445f13309e286c93ccf64febcd
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2024-04-04 23:30:55 +0900

    secrity config basic

--- securitytest/src/main/java/com/security/hellosecurity/auth/service/UserService.java
+++ securitytest/src/main/java/com/security/hellosecurity/auth/service/UserService.java
@@ -4,18 +4,16 @@ import com.security.hellosecurity.auth.domain.entity.Users;
 import com.security.hellosecurity.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Users create(String userId, String password, String email, String authority){
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-        passwordEncoder.encode(password);
         Users user = Users.builder()
                 .userId(userId)
                 .password( passwordEncoder.encode(password))
