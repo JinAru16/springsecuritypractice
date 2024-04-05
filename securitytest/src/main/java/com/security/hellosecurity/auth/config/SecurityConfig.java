64bd5ec1ff6699070cfb7d04072233e1306fe170
JinAru16 <wjdcksdnr11@naver.com>
2024-04-05 11:11:22 +0900

    auth config

--- securitytest/src/main/java/com/security/hellosecurity/auth/config/SecurityConfig.java
+++ securitytest/src/main/java/com/security/hellosecurity/auth/config/SecurityConfig.java
@@ -1,10 +1,66 @@
 package com.security.hellosecurity.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+
+            //SameOrigin 정책을 허용시켜서 iframe 접근이 허용 -> h2이용시에 Forbidden(403)이 나오지 않는다
+            .headers((headersConfigure) -> headersConfigure
+                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        http
+            .authorizeHttpRequests(authorizeRequest ->
+                   authorizeRequest.requestMatchers(
+                           AntPathRequestMatcher.antMatcher("/auth/**"),
+                           AntPathRequestMatcher.antMatcher("/add/**"),
+                           AntPathRequestMatcher.antMatcher("/edit/**")
+                   ).permitAll()
+            );
+
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOriginPattern("*");
+        configuration.setExposedHeaders(Collections.singletonList("*"));
+
+        UrlBasedCorsConfigurationSource urlBaseCors = new UrlBasedCorsConfigurationSource();
+        urlBaseCors.registerCorsConfiguration("/**", configuration);
+        return urlBaseCors;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new  BCryptPasswordEncoder();
+    }
 }
